e1905109e518678479809a85ae19133dd2f682e7
Dieter Baron <dillo@nih.at>
2024-07-14 23:38:20 +0200

    Fix screen layout. Disable mis-assembled function for now.

--- ZX-Spectrum/main-view.s
+++ ZX-Spectrum/main-view.s
@@ -35,7 +35,7 @@
         call read_extended_keys
     }
     call combine_keys
-    call display_keyboard
+    ;call display_keyboard ; XLR8: mistranslates jump
     .if .defined(JOYSTICK_1_DPAD_OFFSET) {
         call display_joystick_1
     }
